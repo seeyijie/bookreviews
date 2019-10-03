5f1141766e932a9801d567e4c6d26e62d00c46aa
yijie <cyberform.jys@gmail.com>
2019-10-03 15:42:17 +0800

    Update the configs for MySQL

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
 from flask import Flask
 import datetime
 from pymongo import MongoClient
-
+import pymysql
 app = Flask(__name__)
 
+# MongoDB
 client = MongoClient('mongodb://localhost:27017/')
+# Create test database in MongoDB if not created
 db = client.test_database
+# Create test collection in test_database if not created in MongoDB
 collection = db.test_collection
+
+# MySQL
+host = "localhost"
+port = 3306
+dbname = "employees"
+user = "yijie"
+password = "password"
+
+conn = pymysql.connect(host, user=user, passwd=password, db=dbname)
 
 
 @app.route('/')
 def hello_world():
-    # post = {"author": "Mike",
-    #         "text": "My first blog post!",
-    #         "tags": ["mongodb", "python", "pymongo"],
-    #         "date": datetime.datetime.utcnow()}
-    #
-    # posts = db.posts
-    # post_id = posts.insert_one(post).inserted_id
-    #
-    # return 'Hello World!\n' + str(post_id)
     return 'Hello world'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
