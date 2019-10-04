2ed40d07010388c6a5c83e540c81a2a810bd0f6b
MikeHo <jinkind_ho@mymail.sutd.edu.sg>
2019-10-04 16:40:36 +0800

    "add env file"

--- app.py
+++ app.py
@@ -1,3 +1,39 @@
+from environs import Env
+from flask import Flask
+import datetime
+from pymongo import MongoClient
+import pymysql
+
+app = Flask(__name__)
+
+# MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+# Create test database in MongoDB if not created
+db = client.test_database
+# Create test collection in test_database if not created in MongoDB
+collection = db.test_collection
+#Setting up environmant variables
+env = Env()
+env.read_env()
+sql_username = env.str("MYSQL_USER", default="root")
+sql_password = env.str("MYSQL_PASSWORD", default="password")
+# MySQL
+host = "localhost"
+port = 3306
+dbname = "employees"
+user = sql_username
+password = sql_password
+
+conn = pymysql.connect(host, user=user, passwd=password, db=dbname)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 from flask import Flask
 import datetime
 from pymongo import MongoClient
@@ -11,12 +47,7 @@ db = client.test_database
 # Create test collection in test_database if not created in MongoDB
 collection = db.test_collection
 
-# MySQL
-host = "localhost"
-port = 3306
-dbname = "employees"
-user = "yijie"
-password = "password"
+
 
 conn = pymysql.connect(host, user=user, passwd=password, db=dbname)
 
