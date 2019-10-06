53ea9857f87a9d0321b907e161d3ef32fe8e9c7e
Khong Jia Wei <khongjw@gmail.com>
2019-10-06 13:12:00 +0800

    updated .env
    please eventually gitignore .env file

--- app.py
+++ app.py
@@ -34,28 +34,28 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True)
-from flask import Flask
-import datetime
-from pymongo import MongoClient
-import pymysql
-app = Flask(__name__)
+# from flask import Flask
+# import datetime
+# from pymongo import MongoClient
+# import pymysql
+# app = Flask(__name__)
 
-# MongoDB
-client = MongoClient('mongodb://localhost:27017/')
-# Create test database in MongoDB if not created
-db = client.test_database
-# Create test collection in test_database if not created in MongoDB
-collection = db.test_collection
-
-
-
-conn = pymysql.connect(host, user=user, passwd=password, db=dbname)
+# # MongoDB
+# client = MongoClient('mongodb://localhost:27017/')
+# # Create test database in MongoDB if not created
+# db = client.test_database
+# # Create test collection in test_database if not created in MongoDB
+# collection = db.test_collection
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+
+# conn = pymysql.connect(host, user=user, passwd=password, db=dbname)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# @app.route('/')
+# def hello_world():
+#     return 'Hello world'
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
