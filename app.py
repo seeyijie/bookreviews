b66d095cd4290dfcc7db8939fc17c8e8c182a661
Khong Jia Wei <khongjw@gmail.com>
2019-10-06 14:12:25 +0800

    updated flask and created index.html, test.html and post.html

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from environs import Env
-from flask import Flask
+from flask import Flask, render_template, request
 import datetime
 from pymongo import MongoClient
 import pymysql
+
+import data             # this imports data.py that I created in the same folder
 
 app = Flask(__name__)
 
@@ -31,31 +33,19 @@ conn = pymysql.connect(host, user=user, passwd=password, db=dbname)
 def hello_world():
     return 'Hello world'
 
+@app.route('/index', methods = ['GET'])         # when you GET request the /index page,
+def index():                                    # this function will run, and
+    return render_template('index.html')        # you will load the index.html file found inside the templates folder
+
+@app.route('/post', methods = ['POST'])         # from index.html, you can send a post request to /post, then this fn happens and post.html is loaded.
+def post():
+    data.exampleDict[request.form['key']] = request.form['value']   # this adjusts a dictionary found in data.py
+    retDict = data.exampleDict
+    return render_template('post.html', retDict = retDict)          # loads post.html, and sends a variable called retDict
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-# from flask import Flask
-# import datetime
-# from pymongo import MongoClient
-# import pymysql
-# app = Flask(__name__)
-
-# # MongoDB
-# client = MongoClient('mongodb://localhost:27017/')
-# # Create test database in MongoDB if not created
-# db = client.test_database
-# # Create test collection in test_database if not created in MongoDB
-# collection = db.test_collection
-
-
-
-# conn = pymysql.connect(host, user=user, passwd=password, db=dbname)
-
-
-# @app.route('/')
-# def hello_world():
-#     return 'Hello world'
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,3 @@
+# This file stores data that are intended to be modified by the users and displayed to the users
+
+exampleDict = {"apple" : "is yum"}
