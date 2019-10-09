d26035806ab96415e9d37b16bf6814b87b1d94da
yijie <cyberform.jys@gmail.com>
2019-10-09 19:28:50 +0800

    Creates a session cookie for the user

--- users/routes.py
+++ users/routes.py
@@ -1,10 +1,9 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, render_template, redirect, session, url_for
 from werkzeug.security import generate_password_hash
 
 from application import db
 from users.models import User
 from users.forms import RegisterForm, LoginForm
-
 
 user_app = Blueprint('user_app', __name__)
 
@@ -23,15 +22,30 @@ def register():
         db.session.add(user)
         db.session.commit()
         return f'User ID: {user.id}'
-    return render_template('/register.html', form=form)
+    return render_template('register.html', form=form)
 
 
 @user_app.route('/login', methods=['GET', 'POST'])
 def login():
+    if session.get('name'):
+        return "You are already logged in!"
+
     form = LoginForm()
     error = None
 
     if form.validate_on_submit():
-        return 'Logged in'
+        user = User.query.filter_by(email=form.email.data).first()
+        session['id'] = user.id
+        session['name'] = user.name
+        # Redirection
+        # '.register' if same file. Otherwise have to specify app.function
+        return redirect(url_for('.register'))
 
-    return render_template('/login.html', form=form, error=error)
+    return render_template('login.html', form=form, error=error)
+
+
+@user_app.route('/logout')
+def logout():
+    session.pop('id')
+    session.pop('name')
+    return redirect(url_for('.login'))
