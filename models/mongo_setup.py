ba69d79d2a0b39105e6b9068fd767d23c70cce87
MikeHo <jinkind_ho@mymail.sutd.edu.sg>
2019-10-15 20:41:48 +0800

    set up on aws

--- models/mongo_setup.py
+++ models/mongo_setup.py
@@ -3,4 +3,4 @@ alias = 'core'
 db = '50043_db'
 
 def global_init():
-    mongoengine.register_connection(alias='core', name = db )
+    mongoengine.register_connection(alias='core', name = db , host='3.16.15.52', port = 27017,username='root', password='password',db = db)
