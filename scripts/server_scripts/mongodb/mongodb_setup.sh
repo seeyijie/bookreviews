532eff11e71df846b9638ce13c3c5747899fca82
Dominic Cordeiro <dominiccordeiro@gmail.com>
2019-10-18 10:34:32 +0800

    script folder restructure

--- scripts/server_scripts/mongodb/mongodb_setup.sh
+++ scripts/server_scripts/mongodb/mongodb_setup.sh
@@ -5,11 +5,11 @@ echo "Installing MongoDB"
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 
 # use this for ubuntu 16.04
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+# echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 # use this for ubuntu 18.04
-# echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get install -y mongodb-org=4.2.0 mongodb-org-server=4.2.0 mongodb-org-shell=4.2.0 mongodb-org-mongos=4.2.0 mongodb-org-tools=4.2.0
 echo "done installing MongoDB"
 
