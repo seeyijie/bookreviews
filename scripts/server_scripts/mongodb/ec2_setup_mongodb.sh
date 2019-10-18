77cfa4ffb0b541d7f6f74c322ac55f5e59ae87a4
Dominic Cordeiro <dominiccordeiro@gmail.com>
2019-10-18 19:24:14 +0800

    mongodb script can now setup mongodb and import dataset

--- scripts/server_scripts/mongodb/ec2_setup_mongodb.sh
+++ scripts/server_scripts/mongodb/ec2_setup_mongodb.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+server_ip="3.18.111.43"
+public_key="experimental_instance.pem"
+username="ubuntu"
+
+# scp datasets to folder in ec2 instance
+scp -i ~/.ssh/$public_key ../../get_data.sh $username@$server_ip:/home/$username
+ssh -i ~/.ssh/$public_key $username@$server_ip 'sudo apt-get -y install unzip'
+ssh -i ~/.ssh/$public_key $username@$server_ip './get_data.sh'
+
+# scp scripts to folder in ec2 instance
+scp -i ~/.ssh/$public_key mongodb_setup.sh $username@$server_ip:/home/$username
+ssh -i ~/.ssh/$public_key $username@$server_ip './mongodb_setup.sh'
--- scripts/server_scripts/mongodb/mongodb_setup.sh
+++ scripts/server_scripts/mongodb/mongodb_setup.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# Yet to test
 echo "Installing MongoDB"
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 
-# use this for ubuntu 16.04
-# echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 # use this for ubuntu 18.04
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
 sudo apt-get -y update
+sudo apt-get install libcurl3 -y
 sudo apt-get install -y mongodb-org=4.2.0 mongodb-org-server=4.2.0 mongodb-org-shell=4.2.0 mongodb-org-mongos=4.2.0 mongodb-org-tools=4.2.0
 echo "done installing MongoDB"
 
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
 echo "Importing dataset to MongoDB"
-sudo mongoimport --db 50043_db --collection books_metadata --file meta_Kindle_Store.json --legacy
+sudo mongoimport --db 50043_db --collection books_metadata --file /home/ubuntu/data_store/meta_Kindle_Store.json --legacy
 
 
