ee671d671523d546b8c49e52e4be1222f086b874
Dominic Cordeiro <dominiccordeiro@gmail.com>
2019-11-02 12:12:55 +0800

    edited flask scripts

--- scripts/server_scripts/flask/ec2_setup_flask.sh
+++ scripts/server_scripts/flask/ec2_setup_flask.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-server_ip="3.14.86.230"
-public_key="experimental_instance.pem"
+server_ip="3.17.147.191"
+public_key="flaskserver.pem"
 username="ubuntu"
-
-wget -c https://www.dropbox.com/s/6g4zfii8f0d7yny/bookreviews.zip?dl=0 -O bookreviews.zip
-sudo apt-get install -y unzip
-unzip bookreviews.zip
 
 scp -i ~/.ssh/$public_key flask_setup.sh $username@$server_ip:/home/$username
 ssh -i ~/.ssh/$public_key $username@$server_ip 'sudo ./flask_setup.sh'
--- scripts/server_scripts/flask/flask_setup.sh
+++ scripts/server_scripts/flask/flask_setup.sh
@@ -6,6 +6,10 @@ sudo apt-get install -y python3-pip
 sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
 sudo apt-get install -y python3-venv
 
+wget -c https://www.dropbox.com/s/6g4zfii8f0d7yny/bookreviews.zip?dl=0 -O bookreviews.zip
+sudo apt-get install -y unzip
+unzip bookreviews.zip
+
 cd bookreviews
 
 python3 -m venv env
