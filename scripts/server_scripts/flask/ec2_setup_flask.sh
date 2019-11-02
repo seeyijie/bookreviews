3b051d59b46cacecf8944771e8f23579e4fa7120
Dominic Cordeiro <dominiccordeiro@gmail.com>
2019-11-02 12:52:14 +0800

    flask server now runs from script

--- scripts/server_scripts/flask/ec2_setup_flask.sh
+++ scripts/server_scripts/flask/ec2_setup_flask.sh
@@ -5,5 +5,6 @@ public_key="flaskserver.pem"
 username="ubuntu"
 
 scp -i ~/.ssh/$public_key flask_setup.sh $username@$server_ip:/home/$username
+scp -i ~/.ssh/$public_key env_setup.sh $username@$server_ip:/home/$username
 ssh -i ~/.ssh/$public_key $username@$server_ip 'sudo ./flask_setup.sh'
 
