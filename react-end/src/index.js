6e47e6d1124f527a86069f20f8d0192628cf0f99
yijie <cyberform.jys@gmail.com>
2019-11-13 14:23:13 +0800

    Login and registration page now with backend. JWT implemented

--- react-end/src/index.js
+++ react-end/src/index.js
@@ -5,13 +5,14 @@ import Home from './Home';
 import Browse from './Browse';
 import Book from './Book';
 import Log from './Log';
-import Register from './Components/Register'
+import Register from './Components/Register';
+import Signin from './Components/Login';
 
 const routing = (
     <Router>
         <div>
             <Route exact path="/" component={Home} />
-            {/*<Route path="/login" component={Login} />*/}
+            <Route path="/signin" component={Signin} />
             <Route path="/signup" component={Register} />
             <Route path="/browse" component={Browse} />
             <Route path='/books/:bookid' component={Book} />
