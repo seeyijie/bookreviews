7bb7e0596f3783f445008650c6b6949e2ffa9263
yunyi <kangyunyi92@gmail.com>
2019-12-11 20:16:22 +0800

    modified export_databases.sh

--- boto3/export_databases.sh
+++ boto3/export_databases.sh
@@ -4,21 +4,26 @@
 #ssh into each database instance and execute export
 #scp files over to local computer
 
+
+#mysql
 source ./config_files/config_mysql.sh
 echo "Exporting mysql data"
 mysql_server_ip=$server_ip
 mysql_public_key=$public_key
-ssh -i ~/$mysql_public_key ubuntu@$mysql_server_ip "mysqldump -u root 50043_DB > mysql_data.sql"
-scp -i ~/$mysql_public_key ubuntu@$mysql_server_ip:/home/ubuntu/mysql_data.sql . 
+ssh -i ~/.ssh/$mysql_public_key ubuntu@$mysql_server_ip "mysql -u root 50043_DB -e 'select asin, reviewText from reviews' --column-names" > mysql.txt
+echo "Writing data to mysql_data.csv"
+sed 's/\t/,/g' mysql.txt > mysql_data.csv  
+rm mysql.txt
 
-
-
+#mongo
 source ./config_files/config_mongodb.sh
 echo "Exporting mongo data"
 mongo_server_ip=$server_ip
 mongo_public_key=$public_key
-ssh -i ~/$mongo_public_key ubuntu@$mongo_server_ip "mongoexport -d 50043_db -c books_metadata -o mongo_data.json"
-scp -i ~/$mongo_public_key ubuntu@$mongo_server_ip:/home/ubuntu/mongo_data.json . 
+ssh -i ~/.ssh/$mongo_public_key ubuntu@$mongo_server_ip "mongo 50043_db --eval 'db.books_metadata.find({},{asin:1,price:1,_id:0}).forEach(printjson)'" > mongo.txt
+echo "Writing data to mongo_data.json"
+sed '1,4d' mongo.txt > mongo_data.json
+rm mongo.txt
 
 echo "Done"
 
