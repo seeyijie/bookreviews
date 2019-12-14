14467689280456016baf946c300ffa980bcd836b
Khong Jia Wei <khongjw@gmail.com>
2019-12-14 17:27:53 +0800

    next page react updated

--- react-end/src/Browse.js
+++ react-end/src/Browse.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
-import { Header, BrowseAllEntries } from './Components'
-import { Grid, Typography } from '@material-ui/core';
+import { Header, BrowseAllEntries } from './Components';
+import { Grid, Typography, Button } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import axios from 'axios';
 import * as config from './Data/config';
@@ -10,13 +10,22 @@ const styles = () => ({
         marginTop: '50px'
     },
     button: {
-        marginTop: '50px',
+        marginTop: '10px',
         border: 0,
         borderRadius: 5,
     },
     loadtext: {
         marginTop: '50px',
-    }
+        marginBottom: '20px'
+    },
+    leftGrid: {
+        paddingLeft: '50px',
+        paddingTop: '50px'
+    },
+    rightGrid: {
+        paddingLeft: '50px',
+        paddingTop: '50px'
+    },
 });
 
 class Browse extends Component {
@@ -27,13 +36,36 @@ class Browse extends Component {
             isLoading: true,
             booksmetadata: [],
             isSearch: false,
-            searchBooksmetadata: []
+            searchBooksmetadata: [],
+            pageNum: 1,
         }
+
+        this.previousPage = this.previousPage.bind(this);
+        this.nextPage = this.nextPage.bind(this);
+    }
+
+    async previousPage() {
+        await this.setState(prevState => {
+            return {
+                pageNum: prevState.pageNum - 1,
+                isLoading: true
+            }
+        })
+        this.componentDidMount();
+    }
+
+    async nextPage() {
+        await this.setState(prevState => {
+            return {
+                pageNum: prevState.pageNum + 1,
+                isLoading: true
+            }
+        })
+        this.componentDidMount();
     }
 
     componentDidMount() {
-        // const url = `http://127.0.0.1:5000/api/allbooks`
-        const url = `${config.flaskip}/api/allbooks`
+        const url = `${config.flaskip}/api/allbooks/${this.state.pageNum}`
 
         axios.get(url)
             .then(response => {
@@ -54,7 +86,24 @@ class Browse extends Component {
             <Grid container alignItems='center' direction='column'>
                 {this.state.isLoading ? loadingMessage : <BrowseAllEntries booksmetadata={booksmetadata} />}
             </Grid>
-        </Fragment>
+            <Grid container alignItems='center' direction='column'>
+                <Typography variant="subtitle2">
+                    Page {this.state.pageNum}
+                </Typography>
+            </Grid>
+            <Grid className={classes.bigGrid} container direction='row'>
+                <Grid container alignItems='center' direction='column' item md>
+                    <Button onClick={this.previousPage} className={classes.button} variant="contained">
+                        Previous page
+                </Button>
+                </Grid>
+                <Grid container alignItems='center' direction='column' item md>
+                    <Button onClick={this.nextPage} className={classes.button} variant="contained">
+                        Next page
+                </Button>
+                </Grid>
+            </Grid>
+        </Fragment >
     }
 }
 
