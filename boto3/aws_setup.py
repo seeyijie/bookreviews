59eb5e727410ef72fb9bd757e5378ff2a32f3329
Chia Yew Ken <chiayewken@gmail.com>
2019-12-19 22:29:06 +0800

    AWS setup from csv in call_master.py: added chmod 400 for keyfile

--- boto3/aws_setup.py
+++ boto3/aws_setup.py
@@ -97,8 +97,8 @@ def put_keyfile_in_ssh(keyfile):
     if not os.path.exists(folder):
         os.mkdir(folder)
     path_out = os.path.join(folder, keyfile)
-    print(run_shell("chmod 400 {}".format(path_out)))
     shutil.copyfile(keyfile, path_out)
+    print(run_shell("chmod 400 {}".format(path_out)))
 
 
 def main(csv_aws_credentials, region="us-east-1"):
