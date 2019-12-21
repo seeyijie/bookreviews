d38af7095b4cdd113d628d7ec4458b8308ad9309
Chia Yew Ken <chiayewken@gmail.com>
2019-12-22 01:21:47 +0800

    Minor changes

--- boto3/bash_scripts/extract_data/extract_mysql.sh
+++ boto3/bash_scripts/extract_data/extract_mysql.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 
-# install virtualenv to run script
-cd "/home/ubuntu/bookreviews" || exit
-python3 -m venv env
-
-# need to change permissions before running this....
-source env/bin/activate
-pip3 install boto3 fire
+## install virtualenv to run script
+#cd "/home/ubuntu/bookreviews" || exit
+#python3 -m venv env
+#
+## need to change permissions before running this....
+#source env/bin/activate
+pip3 install -q boto3 fire
 
 cd "/home/ubuntu/bookreviews/boto3"
 # extract data from mysql database and send to s3 bucket
